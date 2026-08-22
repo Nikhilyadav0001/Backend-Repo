b4002f69f453781d3928d7c9c69b8e8432f98d4a
Nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-08-22 19:54:26 +0530

    Connection poolling using hikari cp

--- Nikhil jdbc/JdbcAppConnectionPoolingApp13/src/practice/HikariCPConfigrationApp.java
+++ Nikhil jdbc/JdbcAppConnectionPoolingApp13/src/practice/HikariCPConfigrationApp.java
@@ -13,6 +13,7 @@ public class HikariCPConfigrationApp {
 	private static final String SQL_SELECT_QUERRY = "select * from employees";
 
 	public static void main(String[] args) {
+		//reusable connections 
 		HikariConfig config =  new HikariConfig("D:\\java files\\Nikhil jdbc\\JdbcAppConnectionPoolingApp13\\src\\Properties_Files\\HikariCp.properties");
 		try(HikariDataSource dataSource =new HikariDataSource(config)){
 			try(Connection connection =dataSource.getConnection()){

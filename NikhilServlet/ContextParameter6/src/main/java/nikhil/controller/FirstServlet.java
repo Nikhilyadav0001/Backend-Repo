1e7a21d4920ed64a351adc336b15a0fcacbc1fa9
Nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-08-24 21:55:29 +0530

    Context parameter

--- NikhilServlet/ContextParameter6/src/main/java/nikhil/controller/FirstServlet.java
+++ NikhilServlet/ContextParameter6/src/main/java/nikhil/controller/FirstServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class FirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	//for entire project 
 	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

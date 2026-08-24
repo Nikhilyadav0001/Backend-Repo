eac93d4913c691bdba23ef990d80222cf8d5e74a
Nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-08-24 23:18:46 +0530

    hidden form field

--- NikhilServlet/HiddenFromFiledApp/src/main/java/nikhil/controller/SecondServlet.java
+++ NikhilServlet/HiddenFromFiledApp/src/main/java/nikhil/controller/SecondServlet.java
@@ -15,10 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 public class SecondServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+	//hidden form field
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 

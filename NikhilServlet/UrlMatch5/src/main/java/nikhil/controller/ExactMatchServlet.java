283358bebb417973e22d99f660348c0301a35ce1
Nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-08-24 21:51:15 +0530

    URL Match

--- NikhilServlet/UrlMatch5/src/main/java/nikhil/controller/ExactMatchServlet.java
+++ NikhilServlet/UrlMatch5/src/main/java/nikhil/controller/ExactMatchServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/test")
 public class ExactMatchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	//first priorty
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- NikhilServlet/UrlMatch5/src/main/java/nikhil/controller/LongestmatchServlet.java
+++ NikhilServlet/UrlMatch5/src/main/java/nikhil/controller/LongestmatchServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class LongestmatchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	//longest path
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("RequestProcessing :: LongestmatchServlet.doGet()");

2adf66aa67d842d0312a3f1dd408cbf6e91028bc
Nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-08-24 22:27:55 +0530

    working with cookies

--- NikhilServlet/CookieApp/src/main/java/nikhil/controller/CookieFirstServlet.java
+++ NikhilServlet/CookieApp/src/main/java/nikhil/controller/CookieFirstServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 public class CookieFirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	//working with cookie for session management
 	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
